2c24c36d16e68588027031dd4c02c382fffa308a
mteam88 <medele5794@inst.hcpss.org>
2021-08-28 11:42:41 -0400

    testing

---
+++ Qaike-1.3.0/QaikeAI_1.py
@@ -0,0 +1,51 @@
+from QHelper import *
+# Matthew Edelen 2021
+
+from Qepicenter import *
+InvalidAIUserInput = Exception
+#         0,1,2,3,4,5,6,7,8,9,10,11
+lists = [[0,0,0,0,0,0,0,0,0,0,0 ,0], [0,0,0,0,0,0,0,0,0,0,0,0], \
+    [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0]]
+rnd = 1
+tags = inithumans()
+hmncnt = len(tags)
+true_Dice = [True, True, True, True]
+pnlty = 0
+
+for gameloopiter in range(1,50):
+    print('\n' + '-'*60 + '\n\t\tRound ' + str(gameloopiter) + '\n')
+    pnlty, lists, true_Dice = aiturn(lists, true_Dice, pnlty, tags, hmncnt)
+#    input()
+
+    print('\nHuman player wilds:  ')
+    for tagnum, realtag in enumerate(tags):
+        print('\n' + str(tagnum + 1) + ',  ', end=' ')
+
+        try:
+            wildinpt = int(input(realtag + ': '))
+        except ValueError as _:
+            print('Please input a valid integer.')
+            try:
+                wildinpt = int(input(realtag + ': '))
+            except ValueError as _:
+                raise InvalidAIUserInput('FATAL LOGIC ERROR! Program exiting')
+# Previous lines are just user input cleaning... Kind of excessive...
+        tookhmnw, lists = takehumanwild(lists, wildinpt, true_Dice)
+        if tookhmnw:
+            print('I took the wild. ')
+            if tookhmnw[1] == 10:
+                lists == addX(lists, tookhmnw[0], 11)
+                true_Dice[tookhmnw[0]] = False
+                print('I blocked a color.')
+                print(displists(lists))
+                if isgameover(true_Dice):
+                    handlegameover(lists, pnlty, humans, tags)
+        else:
+            print('I did not take the wild. ')
+# Are any new colors blocked?
+        true_Dice = isblocked(true_Dice)
+        if isgameover(true_Dice):
+            handlegameover(lists, pnlty, humans, tags)
+# Is game over?
+        if input('Is the game over?  (Type "Yes" to affirm):  ') == 'Yes':
+            handlegameover(lists, pnlty, hmncnt, tags)
