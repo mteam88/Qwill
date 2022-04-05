41392857c5bd395c23a5e96a6d970f8f6af65d2a
mteam88 <matthew8edelen@gmail.com>
2022-04-05 17:49:42 +0000

    checkpoint

--- main2.py
+++ main2.py
@@ -22,7 +22,7 @@ class Player:
 class Card(list):
     def __init__(self, initlist=[[0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0]], true_Dice=[True, True, True, True], penalty=0):
         super().__init__(initlist)
-        SCORELIST = [0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78]
+        self.SCORELIST = [0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78]
         self.true_Dice = true_Dice
         self.penalty = penalty
 
@@ -39,6 +39,7 @@ class Card(list):
             print('ERROR')
             if not muffled:
                 raise e
+        return self
 
     def scoreCard(self):
         """
@@ -46,9 +47,9 @@ class Card(list):
         lists minus penalty.
         self & self.penalty are used
         """
-        for i in range(4):
-            scr += SCORELIST[lists[i].count(1)]
         scr = 0-self.penalty
+        for i in range(4):
+            scr += self.SCORELIST[self[i].count(1)]
         return scr
 
 class XPlay:
@@ -57,36 +58,57 @@ class XPlay:
         self.isWild = isWild
     def isPossible(self, card):
         '''
-        Takes Card object, returns boolean. True if the play is possible.
+        Accepts Card object, returns boolean. True if the play is possible according to Qwixx rules
         '''
-        pass
+        pempty = [] # Returns all possible plays, but not last (lock) spot. This means this must be added later on.
+        for i, color in enumerate(card):
+            if card.true_Dice[i] is True:
+                last = self.findlastXs(card)[i]
+                for ind, _ in enumerate(color[last+1:11]):
+                    pempty.append((i, ind+last+1))
+        print("pempty: ", pempty)
+        pos = self.position
+        print("pos: ", pos)
+        if pos in pempty:
+            if pos[1] == 10:
+                if card[pos[0]].count(1) >= 5:
+                    return True
+            else:
+                return True
+        else:
+            return False
+
     def getScoring(self, card):
         '''
         Returns various scoring data.
         Returns: (skipped, score incr) tuple
         '''
-        skipped = self.position[1]- findlastXs()[self.position[0]]
-        scoreincr = 0
-        return (skipped,0)
+        #print(self.findlastXs(card))
+        #skipped section
+        skipped = self.position[1]- self.findlastXs(card)[self.position[0]]
+        #scoreincr section
+        scoreincr = Card(initlist=card, true_Dice=card.true_Dice, penalty=card.penalty).addX(self.position).scoreCard()
+        return (skipped,scoreincr)
 
     def disp(self):
         return __dict__
    
     @classmethod
-    def findlastXs():
+    def findlastXs(cls, card):
         """
         findlastXs returns the last (rightmost) occurence of the number '1' in all rows in lists
         """
         final=[]
-        for row in lists:
+        for row in card:
             for i, e in enumerate(row[::-1]):
                 if e == 1:
                     break
             if 1 not in row:
                 result = -1
+                final.append(result)
             else:
                 result = len(row)-i-1
-        final.append(result)
+                final.append(result)
         return final
 
 
@@ -111,7 +133,10 @@ class LeastSkipped(Evaluater):
         for xPlay in self.xPlays: # Looping for ever play added.
             scoringlist.append([xPlay, xPlay.getScoring(card)]) # Initialize list
         scoringlist.sort(key=lambda x: x[1][0]) # Sort by number of spaces skipped.
-        return scoringlist
+        return scoringlist # TODO extend beyond this obviously
+        #return scoringlist[0]
 
 leastSkippedEval = LeastSkipped([XPlay([0, 1], True), XPlay([0, 2], True), XPlay([1, 1], True), XPlay([2, 1], True), XPlay([0, 0], True), XPlay([3, 3], True)])
-print(leastSkippedEval.evalAll(Card()))
+print([x[1] for x in leastSkippedEval.evalAll(Card())])
+#card = Card(initlist=[[1,1,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0]], true_Dice=[True, True, True, True], penalty=0)
+#print(XPlay([0,0], True).isPossible(card))
